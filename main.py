d07b98b6d3b2b71d992932c5f0099a117353be16
voronovmaksim88 <voronovmaksim88@gmail.com>
2025-08-19 23:11:25 +0700

    пробую собрать apk под виндой

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from kivy.app import App
-from kivy.uix.label import Label
 
 class MyApp(App):  # Наш главный класс-приложение, наследуется от App
     def build(self):  # Этот метод создает и возвращает корневой виджет
-        return Label(text='Привет, Kivy!')  # Создаем и возвращаем виджет "Метка" с текстом
+        # Не возвращаем ничего, чтобы Kivy автоматически загрузил my.kv файл
+        pass
 
 # Запускаем наше приложение
 if __name__ == '__main__':
